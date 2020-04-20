beaa68216afbdfe4a27207608a3432c7b0486ade
AkashMishraRF <akmishra@rodanandfields.com>
2020-04-20 11:12:40 +0530

    Google Analytics added

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import ReactGa from "react-ga";
+
 import './Portfolio.css';
 import Dashboard from './Dashboard/Dashboard'
 
 function Portfolio() {
+
+  useEffect(() => {
+    ReactGa.initialize("UA-164036913-1");
+
+    //To Report Page Views
+    ReactGa.pageview("/");
+  }, [])
+
   return (
     <div className="portfolio container-fluid">
       <Dashboard></Dashboard>
